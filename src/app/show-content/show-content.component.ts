6eb14e40edbe080fb69ec2cf445606c9c6e8b871
ksenijam14 <ksenija.mladenovic9@gmail.com>
2023-04-07 18:20:15 +0200

    Second Angular task is finished.

--- src/app/show-content/show-content.component.ts
+++ src/app/show-content/show-content.component.ts
@@ -1,6 +1,20 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ServisService } from '../servis.service';
 import { Employee } from '../models/employee';
+import { ChartComponent } from "ng-apexcharts";
+
+import {
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
 
 @Component({
   selector: 'app-show-content',
@@ -13,6 +27,11 @@ export class ShowContentComponent implements OnInit {
 
   employees: Employee[] = [];
   namesTime: Object[] = [];
+  hours: number[] = [];
+  names: string[] = [];
+
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
 
   ngOnInit(): void {
     this.servis.getEmployeesData().subscribe((res:Employee[])=>{
@@ -32,6 +51,7 @@ export class ShowContentComponent implements OnInit {
       });
      
       this.namesTime.forEach(name => {
+        //this.names.push(name['name']);
         this.employees.forEach(employee => {
           if(name['name'] == employee.EmployeeName){
             let endTime = new Date(employee.EndTimeUtc).getTime();
@@ -40,8 +60,9 @@ export class ShowContentComponent implements OnInit {
           }
         });
         name['workingTime'] = Math.round(name['workingTime']/hour); //converting to hours
+        //this.hours.push(name['workingTime']);
       });
-      
+
       //sorting descending according to total working hours
       this.namesTime.sort((a, b) => {
         if (a["workingTime"] < b["workingTime"]) {
@@ -52,6 +73,33 @@ export class ShowContentComponent implements OnInit {
         }
         return 0;
       });
+
+      this.namesTime.forEach(element => {
+        this.names.push(element['name']);
+        this.hours.push(element['workingTime']);
+      });
+      this.chartOptions = {
+        series: this.hours,
+        chart: {
+          width: 500,
+          type: "pie"
+        },
+        labels: this.names,
+        responsive: [
+          {
+            breakpoint: 480,
+            options: {
+              chart: {
+                width: 300
+              },
+              legend: {
+                position: "bottom"
+              }
+            }
+          }
+        ]
+      };
     })
+   
   }
 }
